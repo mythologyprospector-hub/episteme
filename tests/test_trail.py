2e1ab0f201bdfcd7d3fce69beb60593763677d14
James Stambaugh <mythologyprospector@gmail.com>
2026-09-18 13:47:44 -0400

    Keep trail timestamp test inside store lifetime

--- tests/test_trail.py
+++ tests/test_trail.py
@@ -152,16 +152,16 @@ def test_discovery_trail_reconstructs_closed_loop_deterministically():
             renewed.id,
             "2026-09-18T00:00:07Z",
         )
+        later = build_discovery_trail(
+            store,
+            renewed.id,
+            "2026-09-18T00:00:08Z",
+        )
 
     assert trail == second
     assert trail.to_json() == second.to_json()
     assert trail.to_lineage_json() == second.to_lineage_json()
 
-    later = build_discovery_trail(
-        store,
-        renewed.id,
-        "2026-09-18T00:00:08Z",
-    )
     assert trail.to_json() != later.to_json()
     assert trail.to_lineage_json() == later.to_lineage_json()
 
