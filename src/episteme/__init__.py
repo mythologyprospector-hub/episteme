8dc1b042afcf1d50ff9ae8c33211e3f1e71c0961
James Stambaugh <mythologyprospector@gmail.com>
2026-09-18 13:32:14 -0400

    Export renewed discovery method

--- src/episteme/__init__.py
+++ src/episteme/__init__.py
@@ -28,7 +28,7 @@ from .model import (
     make_record,
     make_relationship,
 )
-from .discovery import detect_expected_gap, discover_explicit_contradictions, question_from_finding
+from .discovery import detect_expected_gap, discover_evaluation_tensions, discover_explicit_contradictions, question_from_finding
 from .store import Store
 from .proposals import predict, propose_experiment, propose_hypothesis, represent_model
 
@@ -62,6 +62,7 @@ __all__ = [
     "make_record",
     "make_relationship",
     "detect_expected_gap",
+    "discover_evaluation_tensions",
     "discover_explicit_contradictions",
     "question_from_finding",
     "ingest_jsonl",
