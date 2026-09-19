4afa0e298da13ab44462f01d06a0ada78daa644f
James Stambaugh <mythologyprospector@gmail.com>
2026-09-19 19:12:18 -0400

    Expose candidate constraint assessments

--- src/episteme/__init__.py
+++ src/episteme/__init__.py
@@ -1,5 +1,6 @@
 """Episteme package."""
 
+from .candidate_assessment import CandidateConstraintAssessment, CandidateConstraintStatus
 from .ingest import ingest_jsonl, ingest_jsonl_file
 from .model import (
     AssessmentTargetKind,
@@ -46,6 +47,8 @@ __version__ = "0.1.0"
 
 __all__ = [
     "SCHEMA_VERSION",
+    "CandidateConstraintAssessment",
+    "CandidateConstraintStatus",
     "AssessmentTargetKind",
     "DiscoveryExpectation",
     "DiscoveryExpectationKind",
