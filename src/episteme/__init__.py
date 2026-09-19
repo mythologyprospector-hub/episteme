69e0b58f9ada6725c0e24077b37a892bca460a63
James Stambaugh <mythologyprospector@gmail.com>
2026-09-19 17:29:22 -0400

    Expose bounded constraint gap discovery

--- src/episteme/__init__.py
+++ src/episteme/__init__.py
@@ -33,7 +33,7 @@ from .model import (
     make_record,
     make_relationship,
 )
-from .discovery import detect_accounting_gap, detect_expected_gap, detect_structural_payload_sequence_gap, detect_structural_sequence_gap, discover_competing_prediction_opportunities, discover_evaluation_tensions, discover_explicit_contradictions, question_from_finding
+from .discovery import detect_accounting_gap, detect_constraint_gap, detect_expected_gap, detect_structural_payload_sequence_gap, detect_structural_sequence_gap, discover_competing_prediction_opportunities, discover_evaluation_tensions, discover_explicit_contradictions, question_from_finding
 from .store import Store
 from .trail import DiscoveryTrail, TrailEntry, build_discovery_trail
 from .proposals import predict, propose_experiment, propose_hypothesis, represent_model
@@ -79,6 +79,7 @@ __all__ = [
     "make_record",
     "make_relationship",
     "detect_accounting_gap",
+    "detect_constraint_gap",
     "detect_expected_gap",
     "detect_structural_sequence_gap",
     "detect_structural_payload_sequence_gap",
